64e261cad5c7a26fce3a821ebb0e379300c61ed9
praise <essiakpevwepraise@gmail.com>
2023-07-29 15:09:01 -0700

    feat: simple api-call using redux toolkit

---
+++ src/App.tsx
@@ -0,0 +1,78 @@
+
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { incremented, amountAdded } from './features/counter /counter-slice'; 
+import { useFetchBreedsQuery } from './dogs/dogs-api-slice';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+const count = useAppSelector((state) => state.counter.value); 
+const dispatch = useAppDispatch(); 
+
+ const { data = [], isFetching } = useFetchBreedsQuery();
+
+function handleClick() {
+  dispatch(incremented()); 
+}
+
+function IncrementBy() {
+  dispatch(amountAdded(3));
+}
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+       <div>
+       count is {count}
+       </div>
+        <button onClick={handleClick}>
+          increment 
+        </button>
+        <button onClick={IncrementBy}>
+         amountAdded
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+        <div>
+          <p>Number of dogs fetched: {data.length}</p>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Picture</th>
+              </tr>
+
+            </thead>
+            <tbody>
+      {data.map((breed) => (
+        <tr key={breed.id}>
+          <td>{breed.name}</td>
+          <td>
+            <img src={breed.image.url}  alt={breed.name} height={250} />
+          </td>
+        </tr>
+      ))}
+
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/features/counter /counter-slice.ts
@@ -0,0 +1,35 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface CounterState {
+    value: number; 
+}
+
+const initialState: CounterState = {
+   value: 0, 
+}; 
+
+
+const counterSlice = createSlice({
+    name: 'counter', 
+    initialState,
+    reducers: {
+
+    // increment
+    incremented(state) {
+        state.value++; 
+    } ,  
+    // decrement 
+    // reset 
+    amountAdded(state, action: PayloadAction<number>) {
+        state.value += action.payload
+
+    }
+
+    }
+
+   
+
+});
+
+export const { incremented, amountAdded } = counterSlice.actions
+export default counterSlice.reducer; 
